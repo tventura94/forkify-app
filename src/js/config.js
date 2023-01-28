c0779bc50f3beb52f37faaf083baa3a7153283b2
tventura94 <tventura94@gmail.com>
2023-01-28 18:10:12 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dbffcfa5-a4c3-41d2-9945-dc66bd16e783';
+export const MODAL_CLOSE_SEC = 2.5;
